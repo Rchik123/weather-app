ef0237da13ccf3dc38330b8962588fb04f846a2b
Rchik123 <rchik20@freeuni.edu.ge>
2023-04-22 21:25:21 +0400

    Add RecyclerView to fragment

--- app/src/main/java/ge/rchikovani/weather_app/TodayFragment.kt
+++ app/src/main/java/ge/rchikovani/weather_app/TodayFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 class TodayFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +14,11 @@ class TodayFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_today, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<RecyclerView>(R.id.rv_today).adapter = TodayAdapter()
     }
 }
